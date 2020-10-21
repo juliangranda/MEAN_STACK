fb60bab52b45b914a69c8f00a7352dfa0bd7f132
juliangranda <juliangranda@hotmail.com>
2020-10-21 17:20:18 -0500

    ciclos y ventanas

---
+++ JS/09-while.js
@@ -0,0 +1,19 @@
+
+var year = 2018;
+while(year <= 2051){
+    console.log(year);
+    year++;
+    if(year == 2030){
+        console.log("uso de break");
+        break;
+    }
+}
+
+
+
+var years = 30;
+
+do{
+    console.log(years);
+    years--;
+}while(years > 25);
---
+++ JS/10-ventanas.js
@@ -0,0 +1,11 @@
+
+//alerta 
+alert("esta es mi alerta");
+
+//confirmacion
+var mi_resultado = confirm("estas seguro de querer continuar");
+console.log(mi_resultado);
+
+//ingreso datos
+var mi_resultado1 = prompt("que edad tienes", 18);
+console.log(mi_resultado1);
