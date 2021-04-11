10ea6d66b1825ddd71c424f06549ad5b39d28333
juliangranda <juliangranda@hotmail.com>
2021-04-11 18:26:47 -0500

    validaciones

--- proyecto-js/js/main.js
+++ proyecto-js/js/main.js
@@ -127,6 +127,19 @@ $(document).ready(function () {
 
     }
 
+    //validacion
+    if(window.location.href.indexOf('contact') > -1){
+
+        $("form input[name='date']").datepicker({
+            dateFormat: 'dd-mm-yyyy'
+        });
+
+        $.validate({
+            lang:'es'
+        });
+    }   
+
+
 
 
 });
