613b7cb98f35499c928ff4ac3b8671cfb98f73af
juliangranda <juliangranda@hotmail.com>
2021-04-11 16:47:15 -0500

    cambiador de colores y login falso

--- proyecto-js/js/main.js
+++ proyecto-js/js/main.js
@@ -54,5 +54,57 @@ $(document).ready(function () {
         
     });
     
+
+    //selector de tema
+    var theme = $("#theme");
+    $("#to-green").click(function(){
+        theme.attr("href", "css/green.css");
+    });
+
+    $("#to-red").click(function(){
+        theme.attr("href", "css/red.css");
+    });
+    $("#to-blue").click(function(){
+        theme.attr("href", "css/blue.css");
+    });
+
+
+    //scroll arriba de la web
+    $('.subir').click(function(e){
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+
+
+
+    //login falso
+
+    $("#login form").submit(function(){
+
+        var form_name = $("#form_name").val();
+        localStorage.setItem("form_name",form_name);
+
+    });
+
+
+    var form_name = localStorage.getItem("form_name");
+
+    if(form_name != null && form_name != "undefined"){
+        var about_parrafo = $("#about p");
+        about_parrafo.html("<br><strong>Bienvenido," + form_name + "<strong>");
+        about_parrafo.append("<a href='#' id= 'logout'> Cerrar sesion </a>");
+
+        $("#login").hide();
+
+
+        $("#logout").click(function(){
+            localStorage.clear();
+            location.reload();
+        });
+    }
+
 });
 
