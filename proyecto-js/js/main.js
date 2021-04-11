262df96736a10f7d648cdb38bdb2e633c7354463
juliangranda <juliangranda@hotmail.com>
2021-04-11 17:28:21 -0500

    acordeon

--- proyecto-js/js/main.js
+++ proyecto-js/js/main.js
@@ -2,58 +2,61 @@ $(document).ready(function () {
 
 
     //slider
-    $('.galeria').bxSlider({
-        mode: 'fade',
-        captions: true,
-        slideWidth: 1200
-    });
+    if(window.location.href.indexOf('index') > -1){
+        $('.galeria').bxSlider({
+            mode: 'fade',
+            captions: true,
+            slideWidth: 1200
+        });
+    }
 
     //posts
-    var posts = [
-        {
-            title: 'Prueba de titulo 1',
-            date: 'publicado el dia' + moment().date() + "de" + moment().format("MMMM") + "del" + moment().format("YYYY"),
-            content: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500'
-        },
-        {
-            title: 'Prueba de titulo 2',
-            date: 'publicado el dia' + moment().date() + "de" + moment().format("MMMM") + "del" + moment().format("YYYY"),
-            content: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500'
-        },
-        {
-            title: 'Prueba de titulo 3',
-            date: 'publicado el dia' + moment().date() + "de" + moment().format("MMMM") + "del" + moment().format("YYYY"),
-            content: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500'
-        },
-        {
-            title: 'Prueba de titulo 4',
-            date: 'publicado el dia' + moment().date() + "de" + moment().format("MMMM") + "del" + moment().format("YYYY"),
-            content: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500'
-        },
-        {
-            title: 'Prueba de titulo 5',
-            date: 'publicado el dia' + moment().date() + "de" + moment().format("MMMM") + "del" + moment().format("YYYY"),
-            content: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500'
-        },
-    ];
-
+    if(window.location.href.indexOf('index') > -1){
+        var posts = [
+            {
+                title: 'Prueba de titulo 1',
+                date: 'publicado el dia' + moment().date() + "de" + moment().format("MMMM") + "del" + moment().format("YYYY"),
+                content: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500'
+            },
+            {
+                title: 'Prueba de titulo 2',
+                date: 'publicado el dia' + moment().date() + "de" + moment().format("MMMM") + "del" + moment().format("YYYY"),
+                content: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500'
+            },
+            {
+                title: 'Prueba de titulo 3',
+                date: 'publicado el dia' + moment().date() + "de" + moment().format("MMMM") + "del" + moment().format("YYYY"),
+                content: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500'
+            },
+            {
+                title: 'Prueba de titulo 4',
+                date: 'publicado el dia' + moment().date() + "de" + moment().format("MMMM") + "del" + moment().format("YYYY"),
+                content: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500'
+            },
+            {
+                title: 'Prueba de titulo 5',
+                date: 'publicado el dia' + moment().date() + "de" + moment().format("MMMM") + "del" + moment().format("YYYY"),
+                content: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500'
+            },
+        ];
+    
     //console.log(posts);
 
-    posts.forEach((item, index) => {
-        var post = `                
-            <article class="post">
-            <h2>${item.title}</h2>
-            <span class="date">${item.date}</span>
-            <p>
-                ${item.content}
-            </p>
-            <a href="#" class="button-more">Leer mas</a>
-        </article>`;
-        
-        $("#posts").append(post);
-        
-    });
-    
+        posts.forEach((item, index) => {
+            var post = `                
+                <article class="post">
+                <h2>${item.title}</h2>
+                <span class="date">${item.date}</span>
+                <p>
+                    ${item.content}
+                </p>
+                <a href="#" class="button-more">Leer mas</a>
+            </article>`;
+            
+            $("#posts").append(post);
+            
+        });
+    }
 
     //selector de tema
     var theme = $("#theme");
@@ -105,6 +108,13 @@ $(document).ready(function () {
             location.reload();
         });
     }
+
+    if(window.location.href.indexOf('about') > -1){
+        $("#acordeon").accordion();
+
+    }
+
+
 
 });
 
